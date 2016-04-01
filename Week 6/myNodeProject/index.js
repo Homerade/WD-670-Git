7305fccbcd556c0a41cd96e9696a5e297295b958
Homerade <mizzsvizz@gmail.com>
2016-04-01 14:21:22 -0400

    4/1 a

--- Week 6/myNodeProject/index.js
+++ Week 6/myNodeProject/index.js
@@ -8,8 +8,16 @@ app.get('/', function(req,res){
 });
 
 app.get('/medical', function(req,res){
-	res.send('<h1>Medical documents</h1><h3>Here you can store all pertinent medical documents and information</h3>');
+	res.send('<h1>Medical documents</h1><h3>Here you can store all pertinent medical documents and information</h3></br>test results</br>doctor&#146s notes');
 });
+
+		app.get('/medical/testresults',function(req,res){
+			res.send('<h1>Test Results</h1><h3>stored here</h3>')
+		});
+
+		app.get('/medical/drnotes',function(req,res){
+			res.send('<h1>Doctor&#146s Notes</h1><h3>Doctor&#146s notes filed here</h3>')
+		});
 
 app.get('/interviews',function(req,res){
 	res.send('<h1>Interviews</h1><h3>Here you can store recorded audio interview files<h3>');
@@ -17,6 +25,11 @@ app.get('/interviews',function(req,res){
 
 app.get('/notes',function(req,res){
 	res.send('<h1>Notes<h1><h3>Here you can file notes related to your research process and findings</h3>');
+});
+
+app.use(function(req,res,next){
+	res.status(404);
+	res.send('404 - Not Found');
 });
 //server
 app.listen(3000, function() {
